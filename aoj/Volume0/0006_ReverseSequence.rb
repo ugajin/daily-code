7d0276f2be01c77b50773ad1a0067e490f906374
ugajin <t.ugajin0804@gmail.com>
2019-06-25 22:27:54 +0900

    day_6

---
+++ aoj/Volume0/0006_ReverseSequence.rb
@@ -0,0 +1,20 @@
+# Reverse Sequence
+#
+# ----------------------------------
+# Question
+#
+# Write a program which reverses a given string str.
+#
+# -----------------------------------
+# Input
+#
+# str (the size of str ≤ 20) is given in a line.
+#
+# -----------------------------------
+# Output
+#
+# Print the reversed str in a line.
+#
+# -----------------------------------
+
+puts gets.chop.reverse
