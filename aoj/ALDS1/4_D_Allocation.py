8a6e6bf33213e0b4f4b236ec3d0a1d56f0b804c5
ugajin <t.ugajin0804@gmail.com>
2019-08-24 03:13:54 +0900

    day_35

--- aoj/ALDS1/4_D_Allocation.py
+++ aoj/ALDS1/4_D_Allocation.py
@@ -1,67 +1,38 @@
-import math
-
-def check(w, p, k):
-  cur_k = 1
+def stackable(pack, tmp_p, k):
   cur_w = 0
-  for wi in w:
-    if cur_w + wi <= p:
-      cur_w += wi
+  trucks = 1
+  for i in range(len(pack)):
+    if tmp_p < pack[i]:
+      return False
+
+    if cur_w + pack[i] <= tmp_p:
+      cur_w += pack[i]
     else:
-      cur_k += 1
-      cur_w = wi
-      if k < cur_k:
+      cur_w = pack[i]
+      trucks += 1
+
+      if k < trucks:
         return False
 
   return True
 
-
 n, k = map(int, input().split())
-w = []
+pack = []
 
 for i in range(n):
-  w.append(int(input()))
+  pack.append(int(input()))
 
-max_w = max(w)
-sum_w = sum(w)
-tmp_p = math.ceil(sum_w / k)
-avg_n = n / k
+left = int(sum(pack) / k)
+right = max(pack) * n
 
-print('max_w: {0}'.format(max_w))
-print('sum_w: {0}'.format(sum_w))
-print('tmp_p: {0}'.format(tmp_p))
-print('avg_n: {0}'.format(avg_n))
-
-cur_w = 0
-cur_n = 0
-cur_k = 1
-cur_point = 0
-for i, wi in enumerate(w):
-  if cur_w + wi <= tmp_p:
-    cur_w += wi
-    cur_n += 1
-
-    if i == len(w)-1 and cur_w <= tmp_p:
-      cur_point += avg_n - cur_n
+while left < right - 1:
+  tmp_p = int((left + right) / 2)
+  if stackable(pack, tmp_p, k):
+    right = tmp_p
   else:
-    cur_point += avg_n - cur_n
-    cur_n = 1
-    cur_k += 1
-    cur_w = wi
+    left = tmp_p
 
-  print('cur_w: {0}'.format(cur_w))
-  print('cur_n: {0}'.format(cur_n))
-  print('cur_k: {0}'.format(cur_k))
-  print('cur_point: {0}'.format(cur_point))
-  print('----------------------')
-
-p = tmp_p
-if 0 < cur_point and k < cur_k:
-  p += math.ceil(cur_point)
-
-if p < max_w:
-  p = max_w
-
-while check(w, p, k) == False:
-  p += 1
-
-print(p)
+if stackable(pack, left, k):
+  print(left)
+else:
+  print(right)
